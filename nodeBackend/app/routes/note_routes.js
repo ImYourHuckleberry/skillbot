aa97aff821b995305d1aefee05afffd425743251
wtkrings <w.tyler.krings@gmail.com>
2019-02-06 17:12:33 -0600

    this better work

--- nodeBackend/app/routes/note_routes.js
+++ nodeBackend/app/routes/note_routes.js
@@ -1,22 +1,88 @@
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+var bodyParser = require('body-parser')
 module.exports = function(app, db) {
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
   app.get("/notes", (req, res) => {
     var dbo = db.db("mydb");
-    dbo.collection("skillDatabase").find().toArray(function(err, result) {
+    dbo
+      .collection("skillDatabase")
+      .find()
+      .toArray(function(err, result) {
         if (err) throw err;
-      
-        res.send(result)
+
+        res.send(result);
         //db.close()
-    })
+      });
   });
 
   app.post("/notes", (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection("notes").insert(note, (err, result) => {
-      if (err) {
-        res.send({ error: "An error has occurred" });
-      } else {
-        res.send(result.ops[0]);
-      }
-    });
+    const user = req.body;
+    console.log(user);
+    console.log("attempted post");
+    
+    isExistingMongoUser(user);
+    // db.collection("skillDatabase").insert(user, (err, result) => {
+    //   if (err) {
+    //     res.send({ error: "An error has occurred" });
+    //   } else {
+    //     res.send(result.ops[0]);
+    //   }
+    // });
   });
 };
+
+const isExistingMongoUser = values => {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("values"+values)
+    var dbo = db.db("mydb");
+    dbo
+      .collection("skillDatabase")
+      .findOne({ name: values.values.name }, function(err, res) {
+        if (err) throw err;
+
+        if (res === null) {
+          enterMongoData(values.values);
+        } else {
+          updateMongoData(values.values);
+        }
+        db.close();
+      });
+  });
+};
+
+function enterMongoData(values) {
+  console.log("new user")
+  console.log(values)
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = { name: values.name, skills: values.skills };
+    dbo.collection("skillDatabase").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
+}
+
+function updateMongoData(values) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    console.log(values.skills);
+    for (let i = 0; i < values.skills.length; i++) {
+      dbo
+        .collection("skillDatabase")
+        .update(
+          { name: values.name },
+          { $addToSet: { skills: values.skills[i] } },
+          function(err, res) {
+            if (err) throw err;
+            db.close();
+          }
+        );
+    }
+  });
+}
--- test/README.md
+++
@@ -1,9 +0,0 @@
-1. Goals:   to make an easily accessible and updatable consultant skills database
-2. Target Groups/Personas:   any employee who wishes to update their skills or find employees who have those skills
-3. Problem being Solved:   No easily obtainable, up-to-date consultant skill list
-4. Solutions:   program a Slack bot to facilitate the collection of consultant skills data and store it in a database
-5. Existing Alternatives:   Matt Henroid has a list of consultants and their skills
-6. Advantages (Unique Value):   Streamlined consultant input utilizing existing Daugherty systems
-7. Success Measures:   does it update and return values
-8. Key Success Factors:   gather user input with slackbot, store input in a searchable database
-9. Big Picture(Epics/Feature) Level ideas:   increased access to up-to-date consultant talent information
